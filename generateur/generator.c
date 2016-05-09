5410bc5c1c593c06b9b0a49f9bf993e4d452306d
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-09 23:36:14 +0200

    Add detect dead-end

--- generateur/generator.c
+++ generateur/generator.c
@@ -35,6 +35,8 @@ void	display_map(t_map *map)
 	    printf("X");
 	  else if (map[i].index == 1 || map[i].index == 2)
 	    printf("\x1B[37m\x1B[44m*\x1B[0m");
+	  else if (map[i].index == 6)
+	    printf("\x1B[37m\x1B[41m*\x1B[0m");
 	  j++;
 	  i++;
 	}
@@ -70,7 +72,7 @@ void	write_map(t_map *map, int fd_file)
     }
 }
 
-t_map	*move_up(t_map *map, int *coord, int *flag)
+t_map	*move_up(t_map *map, int *coord)
 {
   if (map[*coord].line > 2)
     {
@@ -81,13 +83,20 @@ t_map	*move_up(t_map *map, int *coord, int *flag)
 	  *coord = *coord - map[0].width;
 	  map[*coord].index = 1;
 	}
-      if (map[*coord].index == 2)
-	return (map);
     }
   return (map);
 }
 
-t_map	*move_right(t_map *map, int *coord, int *flag)
+void	check_move_up(t_map *map, int *coord)
+{
+  if (map[*coord].line > 2)
+    {
+      if (map[*coord - map[0].width].index == 0 && map[*coord - (map[0].width * 2)].index == 0)
+	*coord = *coord - (map[0].width * 2);
+    }
+}
+
+t_map	*move_right(t_map *map, int *coord)
 {
   int	position;
 
@@ -101,8 +110,6 @@ t_map	*move_right(t_map *map, int *coord, int *flag)
 	  *coord = *coord + 1;
 	  map[*coord].index = 1;
 	}
-      if (map[*coord].index == 2)
-	return (map);
     }
   else if ((*coord < map[0].width -1) && map[*coord].line == 1)
     {
@@ -117,7 +124,24 @@ t_map	*move_right(t_map *map, int *coord, int *flag)
   return (map);
 }
 
-t_map	*move_down(t_map *map, int *coord, int *flag)
+void	check_move_right(t_map *map, int *coord)
+{
+  int	position;
+
+  if (map[*coord].line > 1)
+    {
+      position = (*coord - ((map[*coord].line * map[0].width) - map[0].width));
+      if (position < map[0].width -2 && map[*coord + 1].index == 0 && map[*coord + 2].index == 0)
+	*coord = *coord + 2;
+    }
+  else if ((*coord < map[0].width -1) && map[*coord].line == 1)
+    {
+      if (map[*coord +1].index == 0 && map[*coord +2].index == 0)
+	*coord = *coord + 2;
+    }
+}
+
+t_map	*move_down(t_map *map, int *coord)
 {
   if (map[*coord].line < map[0].height -1)
     {
@@ -128,13 +152,20 @@ t_map	*move_down(t_map *map, int *coord, int *flag)
 	  *coord = *coord + map[0].width;
 	  map[*coord].index = 1;
 	}
-      if (map[*coord].index == 2)
-	return (map);
     }
   return (map);
 }
 
-t_map	*move_left(t_map *map, int *coord, int *flag)
+void	check_move_down(t_map *map, int *coord)
+{
+  if (map[*coord].line < map[0].height -1)
+    {
+      if (map[*coord + map[0].width].index == 0 && map[*coord + (map[0].width * 2)].index == 0)
+	*coord = *coord + (map[0].width * 2);
+    }
+}
+
+t_map	*move_left(t_map *map, int *coord)
 {
   int	position;
 
@@ -148,8 +179,6 @@ t_map	*move_left(t_map *map, int *coord, int *flag)
 	  *coord = *coord -1;
 	  map[*coord].index = 1;
 	}
-      if (map[*coord].index == 2)
-	return (map);
     }
   else if (*coord > 1 && map[*coord].line == 1)
     {
@@ -160,35 +189,138 @@ t_map	*move_left(t_map *map, int *coord, int *flag)
           *coord = *coord - 1;
 	  map[*coord].index = 1;
 	}
-      else
-	*flag = *flag + 1;
     }
   return (map);
+}
+
+void	check_move_left(t_map *map, int *coord)
+{
+  int	position;
+
+  if (map[*coord].line > 1)
+    {
+      position = (*coord - ((map[*coord].line * map[0].width) - map[0].width));
+      if (position > 1 && map[*coord - 1].index == 0 && map[*coord - 2].index == 0)
+	*coord = *coord -2;
+    }
+  else if (*coord > 1 && map[*coord].line == 1)
+    {
+      if (map[*coord -1].index == 0 && map[*coord -2].index == 0)
+	*coord = *coord - 2;
+    }
+}
+
+int	try_up(t_map *map, int coord)
+{
+  if (map[coord].line > 1)
+    {
+      if (map[coord - map[0].width].index == 1)
+	coord = coord - map[0].width;
+    }
+  return (coord);
+}
+
+int	try_right(t_map *map, int coord)
+{
+  int	position;
+
+  if (map[coord].line > 1)
+    {
+      position = (coord - ((map[coord].line * map[0].width) - map[0].width));
+      if (position < map[0].width -1 && map[coord + 1].index == 1)
+	coord = coord + 1;
+    }
+  else if ((coord < map[0].width -1) && map[coord].line == 1)
+    {
+      if (map[coord +1].index == 1)
+	coord = coord + 1;
+    }
+  return (coord);
+}
+
+int	try_down(t_map *map, int coord)
+{
+  if (map[coord].line < map[0].height)
+    {
+      if (map[coord + map[0].width].index == 1)
+	coord = coord + map[0].width;
+    }
+  return (coord);
+}
+
+int	try_left(t_map *map, int coord)
+{
+  int	position;
+
+  if (map[coord].line > 1)
+    {
+      position = (coord - ((map[coord].line * map[0].width) - map[0].width));
+      if (position > 1 && map[coord - 1].index == 1)
+	coord = coord -1;
+    }
+  else if (coord > 1 && map[coord].line == 1)
+    {
+      if (map[coord -1].index == 1)
+	coord = coord - 1;
+    }
+  return (coord);
+}
+
+int	previous_star(t_map *map, int coord)
+{
+  coord = try_up(map, coord);
+  coord = try_right(map, coord);
+  coord = try_down(map, coord);
+  coord = try_left(map, coord);
+  return (coord);
+}
+
+int	dead_end(t_map *map, int coord)
+{
+  int	up;
+  int	right;
+  int	down;
+  int	left;
+
+  up = coord;
+  right = coord;
+  down = coord;
+  left = coord;
+  check_move_up(map, &up);
+  check_move_right(map, &right);
+  check_move_down(map, &down);
+  check_move_left(map, &left);
+  if (up == coord && right == coord && down == coord && left == coord)
+    {
+      printf("dead_end\n");
+      /*coord = previous_star(map, coord);*/
+    }
+  return (coord);
 }
 
 t_map	*generator(t_map *map, int fd_file)
 {
   int	coord;
   int	direction;
-  int	flag;
 
   coord = 0;
-  flag = 0;
   while (map[coord].index != 2)
     {
       direction = rand() % 4;
       if (direction == UP)
-	map = move_up(map, &coord, &flag);
+	map = move_up(map, &coord);
       if (direction == RIGHT)
-	map = move_right(map, &coord, &flag);
+	map = move_right(map, &coord);
       if (direction == DOWN)
-	map = move_down(map, &coord, &flag);
+	map = move_down(map, &coord);
       if (direction == LEFT)
-	map = move_left(map, &coord, &flag);
-      if (flag >= 4)
-	map = go_previous_star(map, &coord);
+	map = move_left(map, &coord);
+      coord = dead_end(map, coord);
+      map[coord].index = 6;
       display_map(map);
+      map[coord].index = 1;
       printf("\n\n");
+      usleep(100000);
     }
   return (map);
 }
