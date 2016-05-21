101f1d895f8aa3977862dc0eaf6807009715fe3a
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-21 19:39:36 +0200

    perf.sh add test on largeur and profondeur

--- perf.sh
+++ perf.sh
@@ -1,1 +1,31 @@
-!/bin/bash
+#!/bin/bash
+make re
+clear
+echo "Largeur solver performance :"
+echo "Maze size 25x25"
+echo
+time ./largeur/solver ./mazes/25x25
+echo
+echo "See above the time for largeur resolution '25x25'"
+sleep 6
+clear
+echo "Now maze size 1000x1000"
+echo
+time ./largeur/solver ./mazes/1000x1000
+echo
+echo "See above the time for largeur resolution '1000x1000'"
+sleep 6
+clear
+echo "Profondeur solver performance :"
+echo "Maze size 25x25"
+echo
+time ./profondeur/solver ./mazes/25x25
+echo
+echo "See above the time for profondeur resolution '200x200'"
+sleep 6
+clear
+echo "Now maze size 1000x1000"
+echo
+time ./profondeur/solver ./mazes/1000x1000
+echo
+echo "See above the time for profondeur resolution '1000x1000'"
