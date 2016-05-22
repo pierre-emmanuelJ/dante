e7ced1bc84f49bee65acb526ebc65fc9c1c90467
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-22 18:36:52 +0200

    maze

--- perf.sh
+++ perf.sh
@@ -16,6 +16,20 @@ echo
 echo "See above the time for largeur resolution '1000x1000'"
 sleep 6
 clear
+
+echo "Empty maze 35x45"
+echo
+time ./largeur/solver ./mazes/empty_35x45
+echo
+sleep 6
+clear
+echo "One wall on maze of 35x45"
+echo
+time ./largeur/solver ./mazes/one_wall_35x45
+echo
+sleep 6
+
+clear
 echo "Profondeur solver performance :"
 echo "Maze size 25x25"
 echo
@@ -33,4 +47,18 @@ sleep 6
 clear
 make fclean
 clear
+
+echo "Empty maze 35x45"
+echo
+time ./profondeur/solver ./mazes/empty_35x45
+echo
+sleep 6
+clear
+echo "One wall on maze of 35x45"
+echo
+time ./profondeur/solver ./mazes/one_wall_35x45
+echo
+sleep 6
+clear
+
 echo "thanks for watching"
